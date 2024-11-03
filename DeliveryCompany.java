4eab1d5ad59ed5667f35615c7d7d5e9036ab8101
David Martínez Bergantiño <dmartinezb09@educarex.es>
2024-11-03 20:06:38 +0100

    feat: Subida archivos

--- DeliveryCompany.java
+++ DeliveryCompany.java
@@ -4,20 +4,19 @@ import java.util.*;
  * Model the operation of a taxi company, operating different
  * delivery persons. This version operates a single type of delivery person.
  * 
- * @author David J. Barnes and Michael Kölling
+ * @author David J. Barnes
  * @version 2024.10.07 DP classes
  */
-public class DeliveryCompany  
-{
+public class DeliveryCompany {
     private String name;  //nombre de la compañía
-    private List <DeliveryPerson> deliveryPersons;
+    private List<DeliveryPerson> deliveryPersons;
     private WareHouse wareHouse;
     private int posDeliveryPersonLibre;
+
     /**
      * Constructor for objects of class DeliveryCompany
      */
-    public DeliveryCompany(String name)
-    {
+    public DeliveryCompany(String name) {
         this.name = name;
         deliveryPersons = new ArrayList<>();
         wareHouse = new WareHouse();
@@ -27,46 +26,45 @@ public class DeliveryCompany
     /**
      * @return The name of the company.
      */
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
     /**
      * @return The list of delivery persons.
      */
-    public List<DeliveryPerson> getDeliveryPersons()
-    {       
+    public List<DeliveryPerson> getDeliveryPersons() {
         return deliveryPersons;
     }
 
     /**
      * @return The list of orders.
      */
-    public List<Order> getOrders()
-    {
+    public List<Order> getOrders() {
         return wareHouse.getOrders();
     }
 
     /**
-     * @param Add a new delivery person.
+     * Add a new delivery person.
+     * @param dp The new delivery person.
      */
-    public void addDeliveryPerson(DeliveryPerson dp)
-    { 
+    public void addDeliveryPerson(DeliveryPerson dp) {
         deliveryPersons.add(dp);
+        System.out.println("Added delivery person: " + dp);
     }
 
     /**
-     * Add a new order in the whare house of the company.
+     * Add a new order in the warehouse of the company.
      * @param order The new order.
      */
-    public void addOrder(Order order)
-    {
+    public void addOrder(Order order) {
         wareHouse.addOrder(order);
+        System.out.println("Order added: " + order);
+        System.out.println("Current orders in warehouse: " + wareHouse.getOrders().size());
     }
 
     /**
-     * Find a the most closed free delivery person to the whare house's location, if any.
+     * Find the most closed free delivery person to the warehouse's location, if any.
      * @return A free delivery person, or null if there is none.
      */
     private DeliveryPerson getDeliveryPerson() {
@@ -77,9 +75,9 @@ public class DeliveryCompany
         boolean salir = false;
         int i = 0;
         ComparadorNombreDeliveryPerson comparador = new ComparadorNombreDeliveryPerson();
-    
+
         posDeliveryPersonLibre = -1; // Reset the position
-    
+
         while (i < deliveryPersons.size() && !salir) {
             repLibre1 = deliveryPersons.get(i).isFree();
             if (repLibre1) {
@@ -102,11 +100,11 @@ public class DeliveryCompany
             }
             i++;
         }
-    
+
         if (posDeliveryPersonLibre == -1) {
             return null;
         }
-    
+
         return deliveryPersons.get(posDeliveryPersonLibre);
     }
 
@@ -117,14 +115,14 @@ public class DeliveryCompany
      */
     public boolean requestPickup(Order order) {
         boolean solicita = false;
-        
+
         DeliveryPerson dp = getDeliveryPerson();
         if (dp != null) {
             wareHouse.addOrder(order);
             dp.setPickupLocation(wareHouse.getLocation());
             solicita = true;
         }
-        
+
         return solicita;
     }
 
@@ -133,12 +131,15 @@ public class DeliveryCompany
      * @param dp The delivery person at the pickup point.
      */
     public void arrivedAtPickup(DeliveryPerson dp) {
-        List<Order> orders = wareHouse.getOrders();
+        System.out.println("ArriveAtPickup");
+        List<Order> orders = wareHouse.getOrders(); 
+        System.out.println(orders);
+        System.out.println("Orders in warehouse: " + orders.size());
         if (orders.isEmpty()) {
             throw new IllegalStateException("No orders available for pickup.");
         }
     
-        int lastIndex = orders.size() - 1;
+        int lastIndex = orders.size() -1;
         Order orderToPickup = orders.get(lastIndex);
         dp.pickup(orderToPickup);
         orderToPickup.setDeliveryPersonName(dp.getName());
@@ -148,7 +149,7 @@ public class DeliveryCompany
     }
 
     /**
-     * A delivery person has arrived at a orders's destination.
+     * A delivery person has arrived at an order's destination.
      * @param dp The delivery person at the destination.
      * @param order The order being dropped off.
      */
